38d3d931eb7dcaee8f8d36958c90be933c4241c4
NataliaDrause <nataliadrause@gmail.com>
2023-03-28 19:00:48 +0200

    add sticky navigaion and smooth scroll

--- js/script.js
+++ js/script.js
@@ -11,6 +11,42 @@ btnNavEl.addEventListener('click', function() {
   headerEl.classList.toggle('nav-open');
 });
 
+// SMOOTH SCROLL FOR SAFARI //
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link){
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    // Scrollback to top
+    if (href === "#") {
+      window.scrollTo({ top:0, behavior: "smooth"});
+    }
+    if (href !== "#" && href.startsWith('#')) {
+      document.querySelector(href).scrollIntoView({behavior: "smooth"});
+    }
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+})
+
+// STICKY NAVIGATION //
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  if(!ent.isIntersecting) {
+    document.body.classList.add('sticky');
+  }
+  if(ent.isIntersecting) {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px"
+})
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
